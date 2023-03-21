278b121d4da4f854796aa61249fda56a06a1bbca
Poldi Nguyen <anh.nguyen8@sotatek.com>
2023-03-22 00:23:42 +0700

    Initial Commit

---
+++ TapjoySDK/TapjoySDK.h
@@ -0,0 +1,18 @@
+//
+//  TapjoySDK.h
+//  TapjoySDK
+//
+//  Created by Poldi on 22/03/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TapjoySDK.
+FOUNDATION_EXPORT double TapjoySDKVersionNumber;
+
+//! Project version string for TapjoySDK.
+FOUNDATION_EXPORT const unsigned char TapjoySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TapjoySDK/PublicHeader.h>
+
+
